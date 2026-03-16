c1e4aa28dd6d38f1cfa292aca091114d5a419d27
jbetanof <jbetanof@icloud.com>
2026-03-16 12:40:00 +0000

    add index.html and css files

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,7 +17,7 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+    createPlatform(100, -50, canvas.width + 100, 50); // top wall
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
@@ -27,20 +27,24 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(500, 0, 20, 290);
+    createPlatform(1350, 400, 50, 50, "hotpink");
 
 
 
 
     // TODO 3 - Create Collectables
-
-
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 100);
+    createCannon("right", 300, 2000);
 
 
     
